224552ff0d2f1f1387e832397377c67c3cfd943c
Suezp <suzhipeng@zjmx.com>
2022-04-06 16:11:43 +0800

    暴露webpack、引入路由

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Game from './game/game';
+import Home from './home/home'
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 
-function Ga (props) {
-  return (
-    <div className="inlineBox" >{props.name}</div>
-  )
-}
+// function Ga (props) {
+//   return (
+//     <div className="inlineBox" >{props.name}</div>
+//   )
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Ga name="React" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Game name="组件传参" left = {
-            <Ga name="left" />
-          } >
-          <div>没错我就是 slot</div>
-        </Game>
-      </header>
-    </div>
-  );
+      <HashRouter>
+          <Routes>
+              {/* 路由精确匹配"/home"，跳转Home页面 */}
+              <Route exact path="/home" element={<Home />} />
+              {/* 路由精确匹配"/Game"，跳转Login页面 */}
+              <Route exact path="/game" element={<Game />} />
+              {/* 未匹配，则跳转Game页面 */}
+              <Route path="*" element={<Navigate to="/Game" />} />
+          </Routes>
+      </HashRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/home/home.js
@@ -0,0 +1,16 @@
+// import { Button } from 'antd'
+import './home.css'
+
+function Home() {
+
+    return (
+        <div className="P-home">
+            <h1>Home Page</h1>
+            <div className="ipt-con">
+                <button>返回登录</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
